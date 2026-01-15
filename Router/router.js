e05405a6efcf0fbb7bff90feed219e44f37e68c0
John Tharu <johntharu24@gmail.com>
2026-01-16 02:20:26 +0545

    Homepage and Addlog page created

--- Router/router.js
+++ Router/router.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { homepage, loginpage } from "../controller/controller.js";
+import { addlog, homepage, loginpage } from "../controller/controller.js";
 
 const router = Router();
 
 router.route("/").get(homepage);
 
 router.route("/login").get(loginpage);
+
+router.route("/addlog").get(addlog);
 
 export const routerdata = router;
--- controller/controller.js
+++ controller/controller.js
@@ -2,6 +2,10 @@ export const loginpage = (req, res) => {
   res.render("loginpage");
 };
 
-export const homepage = (req,res) => {
+export const homepage = (req, res) => {
   res.render("homepage");
+};
+
+export const addlog = (req, res) => {
+  res.render("addlog");
 };
